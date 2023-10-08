7f91c8eaa6f3dd87a46807bee96b95362ac2970c
Lord Divan <darveter667@gmail.com>
2023-10-08 23:32:25 +0300

    капзда, давай по новой

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 // getting window el
 const TETRIS_WINDOW_EL = document.querySelector(".tetris-window")!;
 const TETRIS_WINDOW_RECT = TETRIS_WINDOW_EL.getBoundingClientRect();
+const TETRIS_WINDOW_BORDER_WIDTH = 0;
 
-// general width, height settings
-const WINDOW_WIDTH = TETRIS_WINDOW_RECT.width;
-const WINDOW_HEIGHT = TETRIS_WINDOW_RECT.height;
+// general width, height window-settings
+const WINDOW_WIDTH = TETRIS_WINDOW_RECT.width - TETRIS_WINDOW_BORDER_WIDTH;
+const WINDOW_HEIGHT = TETRIS_WINDOW_RECT.height - TETRIS_WINDOW_BORDER_WIDTH;
+
+console.log(WINDOW_HEIGHT)
 
 const CEIL_WIDTH = WINDOW_WIDTH / 10;
 const CEIL_HEIGHT = WINDOW_HEIGHT / 20;
+const CEIL_BORDER_WIDTH = 5;
 
-let lastID = 1;
+let lastID = 0;
 
 interface Tetromino {
   color: string;
@@ -66,8 +70,12 @@ const TETROMINO_TEMPLATES: Tetromino[] = [
   },
 ];
 
-function getCSSProperty(key: string, value: string | number, measure?: string): string {
-  return `${key}:${value}${measure ? measure : ""};`
+function getCSSProperty(
+  key: string,
+  value: string | number,
+  measure?: string
+): string {
+  return `${key}:${value}${measure ? measure : ""};`;
 }
 
 function getRandomTetrominoTemplate(tArray: Tetromino[]): Tetromino {
@@ -80,25 +88,70 @@ function createRandomTetromino(tArray: Tetromino[]): HTMLElement {
 
   const tetromino = document.createElement("div");
   tetromino.classList.add("tetromino");
-  tetromino.style.cssText += getCSSProperty("height", CEIL_HEIGHT * tetrominoTemplate.scheme.length, "px")
-  tetromino.style.cssText += getCSSProperty("width", CEIL_WIDTH * tetrominoTemplate.scheme[0].length, "px")
-  tetromino.id = lastID.toString()
+  tetromino.style.cssText += getCSSProperty(
+    "height",
+    CEIL_HEIGHT * tetrominoTemplate.scheme.length,
+    "px"
+  );
+  tetromino.style.cssText += getCSSProperty(
+    "width",
+    CEIL_WIDTH * tetrominoTemplate.scheme[0].length,
+    "px"
+  );
+  lastID += 1;
+  tetromino.id = lastID.toString();
 
   for (let i = 0; i < tetrominoTemplate.scheme.length; i++) {
     for (let j = 0; j < tetrominoTemplate.scheme[i].length; j++) {
       const ceil = document.createElement("div");
-      ceil.style.cssText += getCSSProperty("width", CEIL_WIDTH - 10, "px")
-      ceil.style.cssText += getCSSProperty("height", CEIL_HEIGHT - 10, "px")
+      ceil.style.cssText += getCSSProperty(
+        "width",
+        CEIL_WIDTH - CEIL_BORDER_WIDTH * 2,
+        "px"
+      );
+      ceil.style.cssText += getCSSProperty(
+        "height",
+        CEIL_HEIGHT - CEIL_BORDER_WIDTH * 2,
+        "px"
+      );
       if (tetrominoTemplate.scheme[i][j] === 1) {
         ceil.classList.add("full-ceil");
-        ceil.style.cssText += getCSSProperty("background-color", tetrominoTemplate.color);
-        ceil.style.cssText += getCSSProperty("border-color", tetrominoTemplate.color);
+        ceil.style.cssText += getCSSProperty(
+          "background-color",
+          tetrominoTemplate.color
+        );
+        ceil.style.cssText += getCSSProperty(
+          "border-color",
+          tetrominoTemplate.color
+        );
       } else ceil.classList.add("empty-ceil");
       tetromino.appendChild(ceil);
     }
   }
-  lastID += 1
   return tetromino;
 }
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-TETRIS_WINDOW_EL.appendChild(createRandomTetromino(TETROMINO_TEMPLATES));
+
+function updateCurrentTetrominoPosition(): void {
+  const regex = new RegExp(/\d*/);
+  const currentTetromino = document.getElementById(lastID.toString())!;
+  let currentPadding: any = regex.exec(currentTetromino.style.paddingTop)![0];
+  if (currentPadding === "") currentPadding = 0;
+  else currentPadding = Number(currentPadding);
+  currentTetromino.style.paddingTop = `${currentPadding + CEIL_HEIGHT}px`;
+  console.log("go out")
+}
+
+async function drawCurrentTetromino() {
+  TETRIS_WINDOW_EL.appendChild(createRandomTetromino(TETROMINO_TEMPLATES));
+  for (let i = 1; i < 20; i++) {
+    await sleep(200);
+    updateCurrentTetrominoPosition();
+  }
+}
+
+drawCurrentTetromino()
+/*капзда*/
